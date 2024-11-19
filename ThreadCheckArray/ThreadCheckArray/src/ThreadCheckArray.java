8bf552970287343370871e10bfdbc4858317ee38
robagit13 <117563051+robagit13@users.noreply.github.com>
2024-11-19 20:38:00 +0200

    Update ThreadCheckArray.java

--- ThreadCheckArray/ThreadCheckArray/src/ThreadCheckArray.java
+++ ThreadCheckArray/ThreadCheckArray/src/ThreadCheckArray.java
@@ -3,7 +3,7 @@ public class ThreadCheckArray implements Runnable
 	private boolean flag;
 	private boolean [] winArray;
 	SharedData sd;
-	int[] array;
+	private ArrayList<Integer>array = new ArrayList<>();
 	int b;
 	
 	public ThreadCheckArray(SharedData sd) 
@@ -14,7 +14,7 @@ public class ThreadCheckArray implements Runnable
 			array = sd.getArray();
 			b = sd.getB();
 		}		
-		winArray = new boolean[array.length];
+		winArray = new boolean[array.size()];
 	}
 	
 	void rec(int n, int b)
@@ -26,7 +26,7 @@ public class ThreadCheckArray implements Runnable
 		}	
 		if (n == 1)
 		{
-			if(b == 0 || b == array[n-1])
+			if(b == 0 || b == array.get(n-1))
 			{
 				flag = true;
 				synchronized (sd) 
@@ -34,12 +34,12 @@ public class ThreadCheckArray implements Runnable
 					sd.setFlag(true);
 				}			
 			}
-			if (b == array[n-1])
+			if (b == array.get(n-1))
 				winArray[n-1] = true;
 			return;
 		}
 		
-		rec(n-1, b - array[n-1]);
+		rec(n-1, b - array.get(n-1));
 		if (flag)
 			winArray[n-1] = true;
 		synchronized (sd) 
@@ -51,7 +51,7 @@ public class ThreadCheckArray implements Runnable
 	}
 
 	public void run() {
-		if (array.length != 1)
+		if (array.size() != 1)
 			if (Thread.currentThread().getName().equals("thread1"))
 				rec(array.length-1, b - array[array.length - 1]);
 			else 
